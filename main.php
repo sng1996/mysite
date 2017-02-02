98a5d38fe8bcbf498228d9863d2a14116aa03a1a
Gavrilko Sergei <seregagavrilko@mail.ru>
2017-02-02 12:56:16 +1000

    first commit

---
+++ main.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=divice-width, initial-scale=1.0">
+    <title>Main</title>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="my.css" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+        <div class="navbar-header" style="margin-left: 10vw">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Главная</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a class="toptext" href="#">Продукты</a></li>
+                <li><a class="toptext" href="#">Блог</a></li>
+                <li><a class="toptext" href="#">О нас</a></li>
+                <li><a class="toptext" href="#">Отзывы</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right" style="margin-right: 10vw;">
+                <li><a class="toptext" href="#">Войти</a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
+<div class="img">
+    <div class="img-overlay">
+        <h1 class="maintext">Our mission is to make money</h1>
+        <h1 class="maintext" style="padding-top: 0px">fast and easy</h1>
+    </div>
+</div>
+<div class="followers-block">
+    <h1 class="followers-header">Join Over 2,749,513 Success Seekers</h1>
+    <div class="col-md-10 col-md-offset-2" style="margin-top: 20px;">
+        <div class="col-md-2" style="text-align: center">
+            <img src="static/mail_icon.png" height="50px">
+            <h3 style="color: white;">1,345,356</h3>
+        </div>
+        <div class="col-md-2" style="text-align: center">
+            <img src="static/facebook_icon.png" height="50px">
+            <h3 style="color: white;">1,345,356</h3>
+        </div>
+        <div class="col-md-2" style="text-align: center">
+            <img src="static/twitter_icon.png" height="50px">
+            <h3 style="color: white;">1,345,356</h3>
+        </div>
+        <div class="col-md-2" style="text-align: center">
+            <img src="static/youtube_icon.png" height="50px">
+            <h3 style="color: white;">1,345,356</h3>
+        </div>
+        <div class="col-md-2" style="text-align: center">
+            <img src="static/vk_icon.png" height="50px">
+            <h3 style="color: white;">1,345,356</h3>
+        </div>
+    </div>
+</div>
+<div class="products-block">
+    <h1 class="products-header">What do we do to improve today?</h1>
+    <div class="col-md-6 light_div">
+        <img src="static/man.png" class="products-icon">
+        <h1 class="products-text">Я</h1>
+        <h4 class="products-text">Развитие личности и саморазвитие</h4>
+    </div>
+    <div class="col-md-6 dark_div">
+        <img src="static/team.png" class="products-icon">
+        <h1 class="products-text">Команда</h1>
+        <h4 class="products-text">Развитие личности и саморазвитие</h4>
+    </div>
+    <div class="col-md-6 dark_div">
+        <img src="static/business.png" class="products-icon">
+        <h1 class="products-text">Бизнес</h1>
+        <h4 class="products-text">Развитие личности и саморазвитие</h4>
+    </div>
+    <div class="col-md-6 light_div">
+        <img src="static/trener.png" class="products-icon">
+        <h1 class="products-text">Тренер</h1>
+        <h4 class="products-text">Развитие личности и саморазвитие</h4>
+    </div>
+</div>
+<div class="saying-block">
+    <h1 class="saying-header">Что говорят наши клиенты о нас</h1>
+    <div style="float:left; margin-top:30px;">
+        <div class="col-md-offset-1 col-md-1">
+            <img src="static/avatar.png" class="saying-icon">
+        </div>
+        <div class="col-md-8 col-md-offset-1">
+            <h3 class="saying-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut magna placerat, faucibus ipsum in, dapibus enim. Donec dapibus nibh non sem imperdiet interdum. Curabitur nec quam molestie, viverra urna sit amet, posuere nisi. Duis feugiat luctus lacus vel laoreet. Praesent a mattis mauris. Nunc porta mi nulla, non sodales felis tincidunt id.</h3>
+        </div>
+    </div>
+    <div style="float:right; margin-top:30px;">
+        <div class="col-md-8 col-md-offset-1">
+            <h3 class="saying-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut magna placerat, faucibus ipsum in, dapibus enim. Donec dapibus nibh non sem imperdiet interdum. Curabitur nec quam molestie, viverra urna sit amet, posuere nisi. Duis feugiat luctus lacus vel laoreet. Praesent a mattis mauris. Nunc porta mi nulla, non sodales felis tincidunt id.</h3>
+        </div>
+        <div class="col-md-offset-1 col-md-1">
+            <img src="static/avatar.png" class="saying-icon">
+        </div>
+    </div>
+    <div style="float:left; margin-top:30px;">
+        <div class="col-md-offset-1 col-md-1">
+            <img src="static/avatar.png" class="saying-icon">
+        </div>
+        <div class="col-md-8 col-md-offset-1">
+            <h3 class="saying-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut magna placerat, faucibus ipsum in, dapibus enim. Donec dapibus nibh non sem imperdiet interdum. Curabitur nec quam molestie, viverra urna sit amet, posuere nisi. Duis feugiat luctus lacus vel laoreet. Praesent a mattis mauris. Nunc porta mi nulla, non sodales felis tincidunt id.</h3>
+        </div>
+    </div>
+    <div style="float:right; margin-top:30px;">
+        <div class="col-md-8 col-md-offset-1">
+            <h3 class="saying-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut magna placerat, faucibus ipsum in, dapibus enim. Donec dapibus nibh non sem imperdiet interdum. Curabitur nec quam molestie, viverra urna sit amet, posuere nisi. Duis feugiat luctus lacus vel laoreet. Praesent a mattis mauris. Nunc porta mi nulla, non sodales felis tincidunt id.</h3>
+        </div>
+        <div class="col-md-offset-1 col-md-1">
+            <img src="static/avatar.png" class="saying-icon">
+        </div>
+    </div>
+</div>
+<div class="comp-block">
+    <h1 class="products-header">Компании, с которыми мы работаем</h1>
+    <div class="col-md-3 light_div" style="height: 200px">
+        <img src="static/google.png" class="comp-icon">
+    </div>
+    <div class="col-md-3 dark_div" style="height: 200px">
+        <img src="static/google.png" class="comp-icon">
+    </div>
+    <div class="col-md-3 light_div" style="height: 200px">
+        <img src="static/google.png" class="comp-icon">
+    </div>
+    <div class="col-md-3 dark_div" style="height: 200px">
+        <img src="static/google.png" class="comp-icon">
+    </div>
+    <div class="col-md-3 dark_div" style="height: 200px">
+        <img src="static/google.png" class="comp-icon">
+    </div>
+    <div class="col-md-3 light_div" style="height: 200px">
+        <img src="static/google.png" class="comp-icon">
+    </div>
+    <div class="col-md-3 dark_div" style="height: 200px">
+        <img src="static/google.png" class="comp-icon">
+    </div>
+    <div class="col-md-3 light_div" style="height: 200px">
+        <img src="static/google.png" class="comp-icon">
+    </div>
+</div>
+<script src="jquery.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
