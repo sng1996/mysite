b5ff4504c8445806548dd43cbefe81582d7db7d1
Gavrilko Sergei <seregagavrilko@mail.ru>
2017-02-09 16:06:26 +1000

    add blog

---
+++ blog.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=divice-width, initial-scale=1.0">
+    <title>Main</title>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="my.css" rel="stylesheet">
+</head>
+<body style="background-color: rgba(220, 220, 220, 0.3)">
+<!--<nav class="navbar navbar-default" role="navigation" style="background-color: rgba(211, 211, 211, 0.4)">
+    <div class="container-fluid">
+        <div class="navbar-header" style="margin-left: 2vw">
+            <a class="navbar-brand" href="#">
+                <img alt="Brand" src="/static/business-training.png" height="30px">
+            </a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <!--<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right" style="margin-right: 2vw;">
+                <li><a class="toptext" href="#products">Продукты</a></li>
+                <li><a class="toptext" href="#">Блог</a></li>
+                <li><a class="toptext" href="#about">О нас</a></li>
+                <li><a class="toptext" href="#saying">Отзывы</a></li>
+                <li style="margin-left: 5vw">
+                    <form class="navbar-form navbar-right">
+                        <a href="#" class="btn btn-sm btn-custom">Подписаться</a>
+                    </form>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>-->
+<div style="margin-top: 60px; margin-bottom: 60px; text-align: center">
+    <a href="main.php#about" class="blog-head">О нас</a>
+    <a class="blog-head">Популярные теги</a>
+    <a href="main.php">
+        <img alt="Brand" src="/static/business-training.png" height="30px">
+    </a>
+    <a class="blog-head">Категории</a>
+    <a href="main.php#saying" class="blog-head">Отзывы</a>
+</div>
+<div>
+    <div class="blog">
+        <img src="http://lorempixel.com/400/200/sports" style="width: 30vw">
+        <div style="padding: 20px;">
+            <h2 class="blog-header">What is Lorem Ipsum?</h2>
+            <p class="blog-text">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.</p>
+        </div>
+    </div>
+    <div class="blog">
+        <img src="http://lorempixel.com/400/200/technics" style="width: 30vw">
+        <div style="padding: 20px;">
+            <h2 class="blog-header">What is Lorem Ipsum?</h2>
+            <p class="blog-text">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
+        </div>
+    </div>
+    <div class="blog">
+        <img src="http://lorempixel.com/400/200/food" style="width: 30vw">
+        <div style="padding: 20px;">
+            <h2 class="blog-header">What is Lorem Ipsum?</h2>
+            <p class="blog-text">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.</p>
+        </div>
+    </div>
+</div>
+<div>
+    <div class="blog">
+        <img src="http://lorempixel.com/400/200/business" style="width: 30vw">
+        <div style="padding: 20px;">
+            <h2 class="blog-header">What is Lorem Ipsum?</h2>
+            <p class="blog-text">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.</p>
+        </div>
+    </div>
+    <div class="blog">
+        <img src="http://lorempixel.com/400/200/cats" style="width: 30vw">
+        <div style="padding: 20px;">
+            <h2 class="blog-header">What is Lorem Ipsum?</h2>
+            <p class="blog-text">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.</p>
+        </div>
+    </div>
+    <div class="blog">
+        <img src="http://lorempixel.com/400/200/city" style="width: 30vw">
+        <div style="padding: 20px;">
+            <h2 class="blog-header">What is Lorem Ipsum?</h2>
+            <p class="blog-text">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.</p>
+        </div>
+    </div>
+</div>
+<script src="jquery.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
--- main.php
+++ main.php
@@ -12,7 +12,7 @@
     <div class="img-overlay">
         <nav class="navbar navbar-default" role="navigation">
             <div class="container-fluid">
-                <div class="navbar-header" style="margin-left: 5vw">
+                <div class="navbar-header" style="margin-left: 2vw">
                     <a class="navbar-brand" href="#">
                         <img alt="Brand" src="/static/business-training.png" height="30px">
                     </a>
@@ -20,19 +20,19 @@
 
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav navbar-right" style="margin-right: 10vw;">
-                        <li><a class="toptext" href="#">Продукты</a></li>
-                        <li><a class="toptext" href="#">Блог</a></li>
-                        <li><a class="toptext" href="#">О нас</a></li>
-                        <li><a class="toptext" href="#">Отзывы</a></li>
-                        <li>
+                    <ul class="nav navbar-nav navbar-right" style="margin-right: 2vw;">
+                        <li><a class="toptext" href="#products">Продукты</a></li>
+                        <li><a class="toptext" href="blog.php">Блог</a></li>
+                        <li><a class="toptext" href="#about">О нас</a></li>
+                        <li><a class="toptext" href="#saying">Отзывы</a></li>
+                        <li style="margin-left: 5vw">
                             <form class="navbar-form navbar-right">
-                                <button type="submit" class="btn btn-default">Подписаться</button>
+                                <a href="#" class="btn btn-sm btn-custom">Подписаться</a>
                             </form>
                         </li>
                     </ul>
-                </div><!-- /.navbar-collapse -->
-            </div><!-- /.container-fluid -->
+                </div>
+            </div>
         </nav>
         <h1 class="maintext">Наша миссия</h1>
         <h1 class="maintext" style="padding-top: 0px">зарабатывать деньги</h1>
@@ -43,30 +43,31 @@
     <h1 class="followers-header">К нам уже присоединились 2,749,513 счастливчиков</h1>
     <div class="col-md-10 col-md-offset-2" style="margin-top: 20px;">
         <div class="col-md-2" style="text-align: center">
-            <img src="static/mail_icon.png" height="50px">
+            <img class="followers-icons" src="static/mail_icon.png">
             <h3 style="color: white;">1,345,356</h3>
         </div>
         <div class="col-md-2" style="text-align: center">
-            <img src="static/facebook_icon.png" height="50px">
+            <img class="followers-icons" src="static/facebook_icon.png">
             <h3 style="color: white;">1,345,356</h3>
         </div>
         <div class="col-md-2" style="text-align: center">
-            <img src="static/twitter_icon.png" height="50px">
+            <img class="followers-icons" src="static/twitter_icon.png">
             <h3 style="color: white;">1,345,356</h3>
         </div>
         <div class="col-md-2" style="text-align: center">
-            <img src="static/youtube_icon.png" height="50px">
+            <img class="followers-icons" src="static/youtube_icon.png">
             <h3 style="color: white;">1,345,356</h3>
         </div>
         <div class="col-md-2" style="text-align: center">
-            <img src="static/vk_icon.png" height="50px">
+            <img class="followers-icons" src="static/vk_icon.png">
             <h3 style="color: white;">1,345,356</h3>
         </div>
     </div>
 </div>
+<a name="products"></a>
 <div class="products-block">
     <h1 class="products-header">Что ты хочешь зарядить сегодня?</h1>
-    <div class="col-md-6 light_div">
+    <!--<div class="col-md-6 light_div">
         <img src="static/man.png" class="products-icon">
         <h1 class="products-text">Заряди себя</h1>
         <h4 class="products-text">Развитие собственных компетенций</h4>
@@ -85,8 +86,24 @@
         <img src="static/trener.png" class="products-icon">
         <h1 class="products-text">Тренер</h1>
         <h4 class="products-text">Развитие личности и саморазвитие</h4>
+    </div>-->
+    <div class="col-md-4 light_div">
+        <img src="static/man.png" class="products-icon">
+        <h1 class="products-text">Заряди себя</h1>
+        <h4 class="products-text">Развитие собственных компетенций</h4>
+    </div>
+    <div class="col-md-4 dark_div">
+        <img src="static/team.png" class="products-icon">
+        <h1 class="products-text">Заряди команду</h1>
+        <h4 class="products-text">Навыки управления людьми</h4>
+    </div>
+    <div class="col-md-4 light_div">
+        <img src="static/business.png" class="products-icon">
+        <h1 class="products-text">Заряди клиентов</h1>
+        <h4 class="products-text">Эффективные взаимоотношения с клиентом</h4>
     </div>
 </div>
+<a name="saying"></a>
 <div class="saying-block">
     <h1 class="saying-header">Отзывы клиентов</h1>
     <div style="float:left; margin-top:30px;">
@@ -147,6 +164,40 @@
     </div>
     <div class="col-md-3 light_div" style="height: 200px">
         <img src="static/google.png" class="comp-icon">
+    </div>
+</div>
+<a name="about"></a>
+<div>
+    <h1 class="products-header">Кто мы?</h1>
+    <h3 class="products-text">Мы команда безнес-тренеров, которая поможет вам стать счастливее</h3>
+    <div class="col-md-offset-3 col-md-3" style="margin-top: 20px; text-align: center">
+        <img src="static/Arkadiy.png" height="350px">
+    </div>
+    <div class="personal-block">
+        <div class="col-md-offset-3 col-md-3" style="text-align: center">
+            <h3>Аркадий Коваленко</h3>
+            <h4 class="products-text">Бизнес-тренер</h4>
+            <img src="static/facebook_icon_green.png" height="20px">
+            <img src="static/linkedin.png" height="20px" style="margin-left:10px">
+            <img src="static/skype_icon.png" height="20px" style="margin-top: 5px; margin-left: 10px">
+        </div>
+        <div class="col-md-3" style="text-align: center">
+            <h3>Роман Рычажков</h3>
+            <h4 class="products-text">Бизнес-тренер</h4>
+            <img src="static/facebook_icon_green.png" height="20px">
+            <img src="static/linkedin.png" height="20px" style="margin-left:10px">
+            <img src="static/skype_icon.png" height="20px" style="margin-top: 5px; margin-left: 10px">
+        </div>
+    </div>
+</div>
+<div class="bottom-block">
+    <div class="bottom-left">
+        <h4>All Rights Reserved</h4>
+    </div>
+    <div class="bottom-right">
+        <h4>Контакты:</h4>
+        <h4>+7(979)797-97-97</h4>
+        <h4>site@zaryadka.biz</h4>
     </div>
 </div>
 <script src="jquery.min.js"></script>
